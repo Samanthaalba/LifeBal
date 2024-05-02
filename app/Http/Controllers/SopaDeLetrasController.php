fe59ca7f3a2c383a0a46af40255187b9e98bec5e
makako15 <murillojuan702@gmail.com>
2024-05-01 22:34:35 -0600

    correccion de sopa de letras

--- app/Http/Controllers/SopaDeLetrasController.php
+++ app/Http/Controllers/SopaDeLetrasController.php
@@ -20,26 +20,32 @@ class SopaDeLetrasController extends Controller
         $matriz = $this->generarSopaDeLetras($palabras);
     
         // Pasa tanto la matriz como las palabras a la vista
-        return view('/juegos/sopa_letras', compact('matriz', 'palabras'));
+        return view('juegos.sopa_letras', compact('matriz', 'palabras'));
     }
-    
 
     private function generarSopaDeLetras($palabras)
     {
-        $tamano = 15;
+        $tamano = 17;
         $matriz = array_fill(0, $tamano, array_fill(0, $tamano, '_'));
+
+        $repetitions = array(); // Contador de repeticiones de cada palabra
+        foreach ($palabras as $palabra) {
+            $repetitions[$palabra] = 0;
+        }
 
         foreach ($palabras as $palabra) {
             $colocada = false;
             $intentos = 0;
             while (!$colocada && $intentos < 100) { // Limita los intentos para evitar bucles infinitos
-                $direccion = rand(0, 2); // 0: horizontal, 1: vertical, 2: diagonal
+                $direccion = rand(0, 1); // 0: horizontal, 1: vertical
                 $fila = rand(0, $tamano - 1);
                 $col = rand(0, $tamano - 1);
 
-                if ($this->puedeColocarPalabra($matriz, $palabra, $fila, $col, $direccion)) {
+                if ($this->puedeColocarPalabra($matriz, $palabra, $fila, $col, $direccion, $tamano)
+                    && $repetitions[$palabra] < 3) { // Limita a 3 repeticiones por palabra
                     $this->colocarPalabra($matriz, $palabra, $fila, $col, $direccion);
                     $colocada = true;
+                    $repetitions[$palabra]++;
                 }
 
                 $intentos++;
@@ -51,61 +57,43 @@ class SopaDeLetrasController extends Controller
         return $matriz;
     }
 
-    private function puedeColocarPalabra(&$matriz, $palabra, $fila, $col, $direccion)
-{
-    $longitud = strlen($palabra);
-    $tamano = count($matriz);
+    private function puedeColocarPalabra(&$matriz, $palabra, $fila, $col, $direccion, $tamano)
+    {
+        $longitud = strlen($palabra);
 
-    for ($i = 0; $i < $longitud; $i++) {
-        // Calcula la posición actual basada en la dirección
-        $posFila = $fila + ($direccion == 1 || $direccion == 2 ? $i : 0);
-        $posCol = $col + ($direccion == 0 || $direccion == 2 ? $i : 0);
-
-        // Verifica si la posición actual está fuera de los límites de la matriz
-        if ($posFila >= $tamano || $posCol >= $tamano|| $posFila < 0 || $posCol < 0) {
-            return false;
+        for ($i = 0; $i < $longitud; $i++) {
+            if ($direccion == 0 && ($col + $i >= $tamano || $matriz[$fila][$col + $i] != '_')) {
+                return false; // No se puede colocar horizontalmente
+            }
+            if ($direccion == 1 && ($fila + $i >= $tamano || $matriz[$fila + $i][$col] != '_')) {
+                return false; // No se puede colocar verticalmente
+            }
         }
 
-        // Verifica solapamiento con otras palabras
-        if ($matriz[$posFila][$posCol] !== '_' && $matriz[$posFila][$posCol] !== $palabra[$i]) {
-            return false;
-        }
+        return true; // La palabra se puede colocar
     }
 
-    return true; // La palabra se puede colocar
-}
-
-
-private function colocarPalabra(&$matriz, $palabra, $fila, $col, $direccion)
-{
-    $palabra = strtoupper($palabra); // Asegura que la palabra esté en mayúsculas
-    $longitud = strlen($palabra);
-    for ($i = 0; $i < $longitud; $i++) {
-        switch ($direccion) {
-            case 0: // Horizontal
-                $matriz[$fila][$col + $i] = $palabra[$i];
-                break;
-            case 1: // Vertical
-                $matriz[$fila + $i][$col] = $palabra[$i];
-                break;
-            case 2: // Diagonal
-                $matriz[$fila + $i][$col + $i] = $palabra[$i];
-                break;
-        }
-    }
-}
-
-
-
-private function rellenarEspaciosVacios(&$matriz)
-{
-    for ($i = 0; $i < count($matriz); $i++) {
-        for ($j = 0; $j < count($matriz[$i]); $j++) {
-            if ($matriz[$i][$j] === '_') {
-                $matriz[$i][$j] = chr(rand(65, 90)); // Genera una letra aleatoria A-Z en mayúsculas
+    private function colocarPalabra(&$matriz, $palabra, $fila, $col, $direccion)
+    {
+        $palabra = strtoupper($palabra); // Asegura que la palabra esté en mayúsculas
+        $longitud = strlen($palabra);
+        for ($i = 0; $i < $longitud; $i++) {
+            if ($direccion == 0) {
+                $matriz[$fila][$col + $i] = $palabra[$i]; // Horizontal
+            } else {
+                $matriz[$fila + $i][$col] = $palabra[$i]; // Vertical
             }
         }
     }
-}
 
+    private function rellenarEspaciosVacios(&$matriz)
+    {
+        for ($i = 0; $i < count($matriz); $i++) {
+            for ($j = 0; $j < count($matriz[$i]); $j++) {
+                if ($matriz[$i][$j] === '_') {
+                    $matriz[$i][$j] = chr(rand(65, 90)); // Genera una letra aleatoria A-Z en mayúsculas
+                }
+            }
+        }
+    }
 }
